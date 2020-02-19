aceceb8061e06f99ccd528b52585f020c09202cb
Sarah Sparrow <sarah.sparrow@oerc.ox.ac.uk>
2020-02-19 21:12:49 +0000

    Script to compress existing extracted NetCDF data in a directory (lossless)

---
+++ compress_extracted_netcdf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+#############################################################################
+# Program : compress_extracted_netcdf.py
+# Author  : Sarah Sparrow
+# Date    : 19/02/2020
+# Purpose : Apply lossless compression to already extracted netcdf files
+#           in the specified directory
+#############################################################################
+
+import os, sys, shutil
+import argparse
+import glob
+
+def compress_netcdf(fname):
+    os.system('nccopy -d 2 -s '+fname+' '+fname+'_compressed')
+    shutil.move(fname+'_compressed',fname)
+
+def main():
+    #Read in the command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", help="The directory containing the netCDF files you want to compress")
+    
+    iter_files=glob.iglob(os.path.join(args.dir,"*.nc"))
+
+    for fname in iter_files:
+        compress_netcdf(fname)
+
+    print('Finished!')
+
+#Washerboard function that allows main() to run on running this file
+if __name__=="__main__":
+  main()
+
+
